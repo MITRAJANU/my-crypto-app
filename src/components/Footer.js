df8d7107bd1429a18c0d77f6f4c84c26a062de14
MITRAJANU <66737656+MITRAJANU@users.noreply.github.com>
2023-03-26 21:23:00 +0530

    1st commit as complete

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <p>Made with ❤️ {"  "}</p>
+      <h4>By Mitrajanu</h4>
+    </div>
+  );
+};
